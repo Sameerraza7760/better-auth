dfb7d1091d2232dbb928d7254079e855173ff26c
sameerraza7760 <sameerraza7760@gmail.com>
2025-09-29 12:20:51 -0700

    feat: add authentication and new About page
    - Added better-auth package for authentication management.
    - Implemented MongoDB adapter for better-auth.
    - Created auth client for handling authentication requests.
    - Added new About page with navigation links.
    - Updated package.json to include new dependencies and devDependencies.
    - Refactored Home page to include navigation links.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,12 @@
+
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
-  function add(a, b) {
+export default function About() {
 
-    
-  }
+
+
+  
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 <div className="flex justify-center gap-2" >
@@ -15,15 +16,16 @@ export default function Home() {
    <Link href={'/about'}  >about</Link>
        
    <Link href={'/Service'}  >Service</Link>
+   
        
    <Link href={'/Contact'}  >Contact</Link>
 
 
-   
+
+
+
 
    
-
- 
 </div>
       
     </div>
